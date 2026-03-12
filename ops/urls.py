2e7a1f115af70793300465fcb782bb5f857582b0
Magali Sol Zapata <maguizap2307@gmail.com>
2026-03-12 02:37:51 -0300

    Add CBV 2

--- ops/urls.py
+++ ops/urls.py
@@ -1,22 +1,35 @@
 from django.urls import path 
-from .views import index, IncidentListView, ServiceListView, PostMortemListView, IncidentDetailView, ServiceDetailView, PostMortemDetailView, incident_form, service_form, postmortem_form
+from . import views
 
 app_name = 'ops'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
     # lists
-    path('incidents/', IncidentListView.as_view(), name='incident_list'),
-    path('services/', ServiceListView.as_view(), name='service_list'),
-    path('postmortem/', PostMortemListView.as_view(), name='postmortem_list'),
+    path('incidents/', views.IncidentListView.as_view(), name='incident_list'),
+    path('services/', views.ServiceListView.as_view(), name='service_list'),
+    path('postmortem/', views.PostMortemListView.as_view(), name='postmortem_list'),
 
     # details
-    path('incidents/<int:pk>/', IncidentDetailView.as_view(), name='incident_detail'),
-    path('services/<int:pk>/', ServiceDetailView.as_view(), name='service_detail'),
-    path('postmortem/<int:pk>/', PostMortemDetailView.as_view(), name='postmortem_detail'),
+    path('incidents/details/<int:pk>', views.IncidentDetailView.as_view(), name='incident_detail'),
+    path('services/details/<int:pk>', views.ServiceDetailView.as_view(), name='service_detail'),
+    path('postmortem/details/<int:pk>', views.PostMortemDetailView.as_view(), name='postmortem_detail'),
+
+    # update
+    path('incidents/update/<int:pk>', views.IncidentUpdateView.as_view(), name='incident_update'),
+    path('services/update/<int:pk>', views.ServiceUpdateView.as_view(), name='service_update'),
+    path('postmortem/update/<int:pk>', views.PostMortemUpdateView.as_view(), name='postmortem_update'),
+
+    # delete
+    path('incidents/delete/<int:pk>', views.IncidentDeleteView.as_view(), name='incident_delete'),
+    path('services/delete/<int:pk>', views.ServiceDeleteView.as_view(), name='service_delete'),
+    path('postmortem/delete/<int:pk>', views.PostMortemDeleteView.as_view(), name='postmortem_delete'),
 
     # forms
-    path('incidents/form', incident_form, name='incident_form'),
-    path('services/form', service_form, name='service_form'),
-    path('postmortem/form', postmortem_form, name='postmortem_form')
+    #path('incidents/form', incident_form, name='incident_form'),
+    #path('services/form', service_form, name='service_form'),
+    #path('postmortem/form', postmortem_form, name='postmortem_form')
+    path('incidents/form', views.IncidentCreateView.as_view(), name='incident_form'),
+    path('services/form', views.ServiceCreateView.as_view(), name='service_form'),
+    path('postmortem/form', views.PostMortemCreateView.as_view(), name='postmortem_form')
 ]
--- ops/views.py
+++ ops/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_list_or_404, redirect
 from .models import Service, Incident, PostMortem
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ServiceForm, IncidentForm, PostMortemForm
 from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
+from django.urls import reverse_lazy
 
 # Home
 def index(request):
@@ -53,7 +55,6 @@ class IncidentDetailView(DetailView):
     model = Incident
     template_name = 'ops/incidents/incident_detail.html'
 
-
 class ServiceDetailView(DetailView):
     model = Service
     template_name = 'ops/services/service_detail.html'
@@ -63,10 +64,45 @@ class PostMortemDetailView(DetailView):
     template_name = 'ops/postmortem/postmortem_detail.html'
 
 
+# update
+class IncidentUpdateView(UpdateView):
+    model = Incident
+    form_class = IncidentForm
+    template_name = 'ops/incidents/incident_form.html'
+    success_url = reverse_lazy('ops:incident_list')
+
+
+class ServiceUpdateView(UpdateView):
+    model = Service
+    form_class = ServiceForm
+    template_name = 'ops/services/service_form.html'
+    success_url = reverse_lazy('ops:service_list')
+
+class PostMortemUpdateView(UpdateView):
+    model = PostMortem
+    form_class = PostMortemForm
+    template_name = 'ops/postmortem/postmortem_form.html'
+    success_url = reverse_lazy('ops:postmortem_list')
+
+
+# delete
+
+class IncidentDeleteView(DeleteView):
+    model = Incident
+    success_url = reverse_lazy('ops:incident_list')
+
+class ServiceDeleteView(DeleteView):
+    model = Service
+    success_url = reverse_lazy('ops:service_list')
+
+class PostMortemDeleteView(DeleteView):
+    model = PostMortem
+    success_url = reverse_lazy('ops:postmortem_list')
+
 
 # forms
 
-@login_required
+"""@login_required
 def incident_form(request):
     if request.method == 'POST':
         form = IncidentForm(request.POST)
@@ -80,10 +116,20 @@ def incident_form(request):
 
     return render(request, 'ops/incidents/incident_form.html', {
         'incidentForm': form
-    })
+    })"""
+
+class IncidentCreateView(LoginRequiredMixin, CreateView):
+    model = Incident
+    form_class = IncidentForm
+    template_name = 'ops/incidents/incident_form.html'
+    success_url = reverse_lazy('ops:incident_list')
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
     
 
-@login_required
+"""@login_required
 def service_form(request):
     if request.method == 'POST':
         form = ServiceForm(request.POST)
@@ -97,9 +143,22 @@ def service_form(request):
 
     return render(request, 'ops/services/service_form.html', {
         'serviceForm': form
-    })
+    })"""
 
-@login_required
+class ServiceCreateView(LoginRequiredMixin, CreateView):
+    model = Service
+    form_class = ServiceForm
+    template_name = 'ops/services/service_form.html'
+    success_url = reverse_lazy('ops:service_list')
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
+    
+
+
+
+"""@login_required
 def postmortem_form(request):
     if request.method == 'POST':
         form = PostMortemForm(request.POST)
@@ -113,4 +172,14 @@ def postmortem_form(request):
     
     return render(request, 'ops/postmortem/postmortem_form.html', {
         'postMortemForm': form
-    })
+    })"""
+
+class PostMortemCreateView(LoginRequiredMixin, CreateView):
+    model = PostMortem
+    form_class = PostMortemForm
+    template_name = 'ops/postmortem/postmortem_form.html'
+    success_url = reverse_lazy('ops:postmortem_list')
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
